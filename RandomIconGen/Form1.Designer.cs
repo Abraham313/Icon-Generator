6996ce1e70303a923c851e78ca26d6c8494e13d8
Traween <tim.vuegen@hotmail.com>
2015-12-17 16:34:00 +0100

    v0.1
    First release

---
+++ RandomIconGen/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace RandomIconGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sizeUpDown = new System.Windows.Forms.NumericUpDown();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.massSizeUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.amountUpDown = new System.Windows.Forms.NumericUpDown();
+            this.locationButton = new System.Windows.Forms.Button();
+            this.massGenerateButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.massSizeUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.amountUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sizeUpDown);
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.generateButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 130);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Generator";
+            // 
+            // sizeUpDown
+            // 
+            this.sizeUpDown.Location = new System.Drawing.Point(147, 41);
+            this.sizeUpDown.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.sizeUpDown.Name = "sizeUpDown";
+            this.sizeUpDown.Size = new System.Drawing.Size(63, 20);
+            this.sizeUpDown.TabIndex = 5;
+            this.sizeUpDown.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(112, 96);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(98, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(112, 67);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(98, 23);
+            this.generateButton.TabIndex = 3;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Size:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.massGenerateButton);
+            this.groupBox2.Controls.Add(this.locationButton);
+            this.groupBox2.Controls.Add(this.amountUpDown);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.massSizeUpDown);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 150);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(221, 83);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mass Generator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Size:";
+            // 
+            // massSizeUpDown
+            // 
+            this.massSizeUpDown.Location = new System.Drawing.Point(59, 22);
+            this.massSizeUpDown.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.massSizeUpDown.Name = "massSizeUpDown";
+            this.massSizeUpDown.Size = new System.Drawing.Size(63, 20);
+            this.massSizeUpDown.TabIndex = 6;
+            this.massSizeUpDown.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Amount:";
+            // 
+            // amountUpDown
+            // 
+            this.amountUpDown.Location = new System.Drawing.Point(59, 54);
+            this.amountUpDown.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.amountUpDown.Name = "amountUpDown";
+            this.amountUpDown.Size = new System.Drawing.Size(63, 20);
+            this.amountUpDown.TabIndex = 8;
+            this.amountUpDown.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // locationButton
+            // 
+            this.locationButton.Location = new System.Drawing.Point(135, 22);
+            this.locationButton.Name = "locationButton";
+            this.locationButton.Size = new System.Drawing.Size(75, 23);
+            this.locationButton.TabIndex = 9;
+            this.locationButton.Text = "Location";
+            this.locationButton.UseVisualStyleBackColor = true;
+            this.locationButton.Click += new System.EventHandler(this.locationButton_Click);
+            // 
+            // massGenerateButton
+            // 
+            this.massGenerateButton.Location = new System.Drawing.Point(135, 51);
+            this.massGenerateButton.Name = "massGenerateButton";
+            this.massGenerateButton.Size = new System.Drawing.Size(75, 23);
+            this.massGenerateButton.TabIndex = 10;
+            this.massGenerateButton.Text = "Generate";
+            this.massGenerateButton.UseVisualStyleBackColor = true;
+            this.massGenerateButton.Click += new System.EventHandler(this.massGenerateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 245);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Icon Gen";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.massSizeUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.amountUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown sizeUpDown;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button massGenerateButton;
+        private System.Windows.Forms.Button locationButton;
+        private System.Windows.Forms.NumericUpDown amountUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown massSizeUpDown;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RandomIconGen/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using RandomIconGen.Properties;
+
+namespace RandomIconGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private Bitmap _icon;
+        private readonly Random _random = new Random(DateTime.Now.Second);
+        private string _location;
+
+
+        #region " Icon Generator Buttons "
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            _icon = GenerateRandomIcon(Convert.ToInt16(sizeUpDown.Value));
+            pictureBox1.Image = _icon;
+        }
+
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (_icon == null) return;
+            using (var sfd = new SaveFileDialog())
+            {
+                sfd.FileName = "icon_" + _random.Next(100, 999) + ".ico";
+                sfd.Filter = Resources.IconFilter;
+                if (sfd.ShowDialog() != DialogResult.OK) return;
+
+                var icon = Icon.FromHandle(_icon.GetHicon());
+                using (var fileStream = new FileStream(sfd.FileName, FileMode.Create))
+                {
+                    icon.Save(fileStream);
+                }
+            }
+        }
+
+        #endregion
+
+        #region " Mass generator Buttons "
+
+        private void locationButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                fbd.RootFolder = Environment.SpecialFolder.Desktop;
+                fbd.SelectedPath = AppDomain.CurrentDomain.BaseDirectory;
+
+                if (fbd.ShowDialog() != DialogResult.OK) return;
+                _location = fbd.SelectedPath;
+            }
+        }
+
+        private void massGenerateButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_location)) return;
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                MassGenerateIcon(Convert.ToInt16(amountUpDown.Value), Convert.ToInt16(sizeUpDown.Value), _location);
+            });
+        }
+
+        #endregion
+
+        #region " Icons Methods "
+
+        private void MassGenerateIcon(int amount, int size, string directory)
+        {
+            var done = 0;
+            while (done <= amount)
+            {
+                var icon = Icon.FromHandle(GenerateRandomIcon(size).GetHicon());
+                using (
+                    var fileStream = new FileStream(directory + "\\icon_" + _random.Next(100, 999) + ".ico", FileMode.Create))
+                {
+                    icon.Save(fileStream);
+                }
+                done++;
+            }
+        }
+
+        private Bitmap GenerateRandomIcon(int size)
+        {
+            var img = new Bitmap(size, size);
+
+            for (var x = 0; x < size; x++)
+            {
+                for (var y = 0; y < size; y++)
+                {
+                    img.SetPixel(x, y, RandomColor());
+                }
+            }
+
+            return img;
+        }
+
+        private Color RandomColor()
+        {
+            return Color.FromArgb(_random.Next(0, 255), _random.Next(0, 255), _random.Next(0, 255));
+        }
+
+        #endregion
+    }
+}
